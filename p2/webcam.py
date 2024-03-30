4f9f1ee73c839e7e576741950b39aa6e48e5124b
Blacklotus88888 <steve.chen@mail.mcgill.ca>
2024-03-30 19:32:18 -0400

    adding 2 synchronous cam feed

--- p2/webcam.py
+++ p2/webcam.py
@@ -13,17 +13,17 @@ from aiortc.contrib.media import MediaPlayer
 ROOT = os.path.dirname(__file__)
 HOST_IP = os.getenv('HOST_IP', "0.0.0.0")
 
-async def index(request):
-    content = open(os.path.join(ROOT, "index.html"), "r").read()
-    return web.Response(content_type="text/html", text=content)
+# async def index(request):
+#     content = open(os.path.join(ROOT, "index.html"), "r").read()
+#     return web.Response(content_type="text/html", text=content)
 
-async def javascript(request):
-    content = open(os.path.join(ROOT, "client.js"), "r").read()
-    return web.Response(content_type="application/javascript", text=content)
+# async def javascript(request):
+#     content = open(os.path.join(ROOT, "client.js"), "r").read()
+#     return web.Response(content_type="application/javascript", text=content)
 
-async def javascript_control(request):
-    content = open(os.path.join(ROOT, "controller.js"), "r").read()
-    return web.Response(content_type="application/javascript", text=content)
+# async def javascript_control(request):
+#     content = open(os.path.join(ROOT, "controller.js"), "r").read()
+#     return web.Response(content_type="application/javascript", text=content)
 
 async def offer(request):
     print(request)  
@@ -44,11 +44,62 @@ async def offer(request):
     if args.play_from:
         player = MediaPlayer(args.play_from)
     else:
-        options = {"framerate": "5", "video_size": "640x360"}
+        options = {"framerate": "25", "video_size": "640x360"}
         if platform.system() == "Darwin":
             player = MediaPlayer("default:none", format="avfoundation", options=options)
         else:
-            player = MediaPlayer("/dev/video0", format="v4l2", options=options)
+            player = MediaPlayer("/dev/video0", format="v4l2", options=options) #check whihc video device is being used
+
+    await pc.setRemoteDescription(offer)
+    for t in pc.getTransceivers():
+        if t.kind == "audio" and player.audio:
+            pc.addTrack(player.audio)
+        elif t.kind == "video" and player.video:
+            pc.addTrack(player.video)
+
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    response = web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+    print("returning")
+
+    # Add CORS headers
+    # response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:4200'  # Replace '*' with your specific allowed origins if necessary
+    response.headers['Access-Control-Allow-Origin'] = '*'  # Replace '*' with your specific allowed origins if necessary
+    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+
+    return response
+
+async def offer2(request):
+    print(request)  
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    @pc.on("iceconnectionstatechange")
+    async def on_iceconnectionstatechange():
+        print("ICE connection state is %s" % pc.iceConnectionState)
+        if pc.iceConnectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    # open media source
+    if args.play_from:
+        player = MediaPlayer(args.play_from)
+    else:
+        options = {"framerate": "25", "video_size": "640x360"}
+        if platform.system() == "Darwin":
+            player = MediaPlayer("default:none", format="avfoundation", options=options)
+        else:
+            player = MediaPlayer("/dev/video2", format="v4l2", options=options) #check whihc video device is being used
 
     await pc.setRemoteDescription(offer)
     for t in pc.getTransceivers():
@@ -135,10 +186,12 @@ if __name__ == "__main__":
 
     app = web.Application()
     app.on_shutdown.append(on_shutdown)
-    app.router.add_get("/", index)
-    app.router.add_get("/client.js", javascript)
-    app.router.add_get("/controller.js", javascript_control)
+    # app.router.add_get("/", index)
+    # app.router.add_get("/client.js", javascript)
+    # app.router.add_get("/controller.js", javascript_control)
     app.router.add_options("/offer", handle_options) 
-    
     app.router.add_post("/offer", offer)
+    app.router.add_options("/offer2", handle_options) 
+    app.router.add_post("/offer2", offer2)
+
     web.run_app(app, host=args.host, port=args.port, ssl_context=ssl_context)
--- src/app/components/webrtc/webrtc.component.ts
+++ src/app/components/webrtc/webrtc.component.ts
@@ -17,7 +17,7 @@ export class WebRTCComponent {
   async negotiate() {
     try {
         this.pc?.addTransceiver('video', { direction: 'recvonly' });
-        this.pc?.addTransceiver('audio', { direction: 'recvonly' });
+        // this.pc?.addTransceiver('audio', { direction: 'recvonly' });
 
         const offer = await this.pc?.createOffer();
         await this.pc?.setLocalDescription(offer);
@@ -95,12 +95,105 @@ export class WebRTCComponent {
     // Hide start button using Angular data binding
     // Call negotiate method
     // Show stop button using Angular data binding
-    document.getElementById('start')!.style.display = 'none';
+    // document.getElementById('start')!.style.display = 'none';
     await this.negotiate();
-    document.getElementById('stop')!.style.display = 'inline-block';
+    // document.getElementById('stop')!.style.display = 'inline-block';
   }
 
   stop() {
+    // Hide stop button using Angular data binding
+    setTimeout(() => {
+      this.pc?.close();
+    }, 500);
+  }
+
+  async negotiate2() {
+    try {
+        this.pc?.addTransceiver('video', { direction: 'recvonly' });
+        // this.pc?.addTransceiver('audio', { direction: 'recvonly' });
+
+        const offer = await this.pc?.createOffer();
+        await this.pc?.setLocalDescription(offer);
+
+        // Wait for ICE gathering to complete
+        await new Promise<void>((resolve) => {
+            if (this.pc?.iceGatheringState === 'complete') {
+                resolve();
+            } else {
+                const checkState = () => {
+                    if (this.pc?.iceGatheringState === 'complete') {
+                        this.pc?.removeEventListener('icegatheringstatechange', checkState);
+                        resolve();
+                    }
+                };
+                this.pc?.addEventListener('icegatheringstatechange', checkState);
+            }
+        });
+
+        console.log("before fetch");
+            
+        let pd = await JSON.stringify({ sdp: this.pc?.localDescription?.sdp, type: this.pc?.localDescription?.type });
+        console.log(pd);
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Access-Control-Allow-Origin', '*'); // Adjust as needed
+      //   headers.append("Access-Control-Allow-Credentials","true ");
+      //   headers.append("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+      //  headers.append("Access-Control-Allow-Headers","Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+        const response = await fetch('http://192.168.0.116:8080/offer2', { //fix this
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*',
+              // 'Access-Control-Allow-Methods': 'POST, OPTIONS',
+              // 'Access-Control-Allow-Headers': 'Content-Type',
+
+            },
+            body: JSON.stringify({ sdp: this.pc?.localDescription?.sdp, type: this.pc?.localDescription?.type })
+        });
+        console.log("after fetch and response is: ", response);
+        const answer = await response.json();
+        console.log("after response and answer is: ", answer);
+        await this.pc?.setRemoteDescription(answer);
+        console.log("after response and answer is: ", answer);
+    } catch (error) {
+        console.error('Error in negotiate:', error);
+        // Handle error as needed
+    }
+}
+
+
+
+  async start2() {
+    const config = { sdpSemantics: 'unified-plan', iceServers: [] as RTCIceServer[] }; // Define iceServers as an empty array initially
+    if ((document.getElementById('use-stun') as HTMLInputElement).checked) {
+      config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
+    }
+    this.pc = new RTCPeerConnection(config);
+
+    this.pc.addEventListener('track', (evt) => {
+      if (evt.track.kind === 'video') {
+        const videoElement = document.getElementById('video2') as HTMLVideoElement | null;
+        if (videoElement) {
+          videoElement.srcObject = evt.streams[0];
+        } else {
+          console.error("Video element not found");
+        }
+        // Update video srcObject using Angular data binding
+      } else {
+        // Update audio srcObject using Angular data binding
+      }
+    });
+
+    // Hide start button using Angular data binding
+    // Call negotiate method
+    // Show stop button using Angular data binding
+    // document.getElementById('start')!.style.display = 'none';
+    await this.negotiate2();
+    // document.getElementById('stop')!.style.display = 'inline-block';
+  }
+
+  stop2() {
     // Hide stop button using Angular data binding
     setTimeout(() => {
       this.pc?.close();
